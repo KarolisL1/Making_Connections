69ecd61f5373f53895c7be0a363c408e933973f9
karolisliepa <Karolis.Liepa@gmail.com>
2022-02-01 09:11:19 -0500

    Making Connections

---
+++ script.js
@@ -0,0 +1,9 @@
+function remove() {
+    var element = document.querySelector(".card-list-item");
+    element.remove();
+}
+
+function change() {
+    var element = document.querySelector(".card-body h1");
+    element.innerHTML = "Karolis Liepa";
+}
